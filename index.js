858ba887b17eb899db2a6f57f005476a9912416b
davepile <pile.dave@gmail.com>
2014-08-23 08:35:51 +1000

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+var PI = 3.142;
+
+exports.circle = {};
+exports.circle.circumference = function(r){
+    return 2 * PI * r;
+};
+exports.circle.perimeter = exports.circle.circumference;
+
+exports.circle.area = function(r){
+    return PI * r * r;
+};
+
+exports.square = {};
+exports.square.perimeter = function(s){
+    return 4 * s;
+};
+exports.square.area = function(s){
+    return s * s;
+};
+
+
